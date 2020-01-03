a13d2d88746cd8f5b4b67275dbe041afcefb514b
alexbaishs <alexbaishs@gmail.com>
2020-01-03 16:43:57 +0900

    1.2

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var gulp         = require('gulp'),
 gulp.task('browser-sync', function() {
 	browserSync.init({
 		server: {
-			baseDir: 'app'
+			baseDir: 'build'
 		},
 		notify: false,
 		// online: false, // Work offline without internet connection
@@ -26,7 +26,7 @@ function bsReload(done) { browserSync.reload(); done(); };
 
 // Custom Styles
 gulp.task('styles', function() {
-	return gulp.src('app/sass/**/*.sass')
+	return gulp.src('src/sass/**/*.sass')
 	.pipe(sass({
 		outputStyle: 'expanded',
 		includePaths: [__dirname + '/node_modules']
@@ -37,7 +37,7 @@ gulp.task('styles', function() {
 		overrideBrowserslist: ['last 10 versions']
 	}))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
-	.pipe(gulp.dest('app/css'))
+	.pipe(gulp.dest('build/css'))
 	.pipe(browserSync.stream())
 });
 
@@ -45,12 +45,11 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		// 'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
-		'app/js/_libs.js', // JS libraries (all in one)
-		'app/js/_custom.js', // Custom scripts. Always at the end
+		'src/js/*.js',
 		])
 	.pipe(concat('scripts.min.js'))
 	.pipe(uglify()) // Minify js (opt.)
-	.pipe(gulp.dest('app/js'))
+	.pipe(gulp.dest('build/js'))
 	.pipe(browserSync.reload({ stream: true }))
 });
 
@@ -59,40 +58,41 @@ var quality = 95; // Responsive images quality
 
 // Produce @1x images
 gulp.task('img-responsive-1x', async function() {
-	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
-		.pipe(newer('app/img/@1x'))
+	return gulp.src('src/img/**/*.{png,jpg,jpeg,webp,raw}')
+		.pipe(newer('build/img/@1x'))
 		.pipe(responsive({
 			'**/*': { width: '50%', quality: quality }
 		})).on('error', function (e) { console.log(e) })
 		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
-		.pipe(gulp.dest('app/img/@1x'))
+		.pipe(gulp.dest('build/img/@1x'))
 });
 // Produce @2x images
 gulp.task('img-responsive-2x', async function() {
-	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
-		.pipe(newer('app/img/@2x'))
+	return gulp.src('src/img/**/*.{png,jpg,jpeg,webp,raw}')
+		.pipe(newer('build/img/@2x'))
 		.pipe(responsive({
 			'**/*': { width: '100%', quality: quality }
 		})).on('error', function (e) { console.log(e) })
 		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
-		.pipe(gulp.dest('app/img/@2x'))
+		.pipe(gulp.dest('build/img/@2x'))
 });
 gulp.task('img', gulp.series('img-responsive-1x', 'img-responsive-2x', bsReload));
 
 // Clean @*x IMG's
 gulp.task('cleanimg', function() {
-	return del(['app/img/@*'], { force: true })
+	return del(['build/img/@*'], { force: true })
 });
 
 // Code & Reload
 gulp.task('code', function() {
-	return gulp.src('app/**/*.html')
+	return gulp.src('src/*.html')
+	.pipe(gulp.dest('build'))
 	.pipe(browserSync.reload({ stream: true }))
 });
 
 // Deploy
 gulp.task('rsync', function() {
-	return gulp.src('app/')
+	return gulp.src('build/')
 	.pipe(rsync({
 		root: 'app/',
 		hostname: 'username@yousite.com',
@@ -107,10 +107,10 @@ gulp.task('rsync', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('app/sass/**/*.sass', gulp.parallel('styles'));
-	gulp.watch(['app/js/_custom.js', 'app/js/_libs.js'], gulp.parallel('scripts'));
-	gulp.watch('app/*.html', gulp.parallel('code'));
-	gulp.watch('app/img/_src/**/*', gulp.parallel('img'));
+	gulp.watch('src/sass/**/*.sass', gulp.parallel('styles'));
+	gulp.watch('src/js/*.js', gulp.parallel('scripts'));
+	gulp.watch('src/*.html', gulp.parallel('code'));
+	gulp.watch('src/img/**/*', gulp.parallel('img'));
 });
 
 gulp.task('default', gulp.parallel('img', 'styles', 'scripts', 'browser-sync', 'watch'));
